01a2b6ec0c40876e8debd4ac6209777677aedd5f
mikozdu451 <56159221+mikozdu451@users.noreply.github.com>
2022-06-26 16:18:47 +0200

    Update AiStateMachine.cs

--- Assets/Scripts/AiStateMachine.cs
+++ Assets/Scripts/AiStateMachine.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+* State machine for enemies AI
+**/
 public class AiStateMachine
 {
     public AiState[] states;
@@ -14,13 +17,13 @@ public class AiStateMachine
         int numStates = System.Enum.GetNames(typeof(AiStateId)).Length;
         states = new AiState[numStates];
     }
-
+    // Register the state of the enemy
     public void RegistarState(AiState state)
     {
         int index = (int)state.GetId();
         states[index] = state;
     }
-
+    // Get state of the enemy
     public AiState GetState(AiStateId stateId)
     {
         int index = (int)stateId;
@@ -31,7 +34,7 @@ public class AiStateMachine
     {
         GetState(currentState)?.Update(agent);
     }
-
+    // Change state of the enmy
     public void ChangeState(AiStateId newState)
     {
         GetState(currentState)?.Exit(agent);
