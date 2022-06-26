6e5b7a58d8fc9576a8b0e1229c8bd90216ff8667
mikozdu451 <56159221+mikozdu451@users.noreply.github.com>
2022-06-26 15:56:54 +0200

    Update AiDeathState.cs

--- Assets/Scripts/AiDeathState.cs
+++ Assets/Scripts/AiDeathState.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/**
+* AI state for enemy to when it dies
+**/
 public class AiDeathState : AiState
 {
+    /**
+    * Whem enemy dies activete ragdoll, change the speed of enemy to 0 so it doesn't move
+    **/
     public void Enter(AiAgent agent)
     {
         agent.ragdoll.ActivateRagdoll();
